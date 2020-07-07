032cb2bd3e68b6203c252d3a46f2dd0b954627a0
kgostic <kategostic@gmail.com>
2020-07-07 12:48:53 -0700

    updated run wrapper

--- 07-07-2020/run_wrapper.sh
+++ 07-07-2020/run_wrapper.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-## These variables need reset every time
-## REMEMBER to insert jobname in last line
-jobpath='02_model_development_kg/PCR_case_model/cluster_scripts/2020_03_18_accumVar_highBeta_sameWk/'
-
-
 ## Pass jobname, output and error paths from wrapper script
-sbatch --job-name=sameWk --export=jj=$jobname,oo=$outpath,ee=$epath,sp=$jobpath ./$jobpath/array_script.sbatch
+sbatch --export=rs=RSCRIPTPATH ./test_script.sbatch
